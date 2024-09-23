1747f4eea399715305f1c37c820d8e4725e606c1
tomwarden <tomlukeprogramming@outlook.com>
2024-09-23 17:23:19 +0100

    Started with sequence logic

--- fibonacci.py
+++ fibonacci.py
@@ -1,2 +1,13 @@
 fibonacci_sequence = []
 
+a = 0; b = 1
+n = new_num
+
+fibonacci_sequence.append(a)
+fibonacci_sequence.append(b)
+
+print(fibonacci_sequence)
+
+new_num = sum(fibonacci_sequence(-n,n))
+print(new_num)
+fibonacci_sequence.append(new_num)
