224a1715ca4b62f6de88677d1695dfd64c5184fb
tomwarden <tomlukeprogramming@outlook.com>
2024-09-23 17:37:05 +0100

    Logic working

--- fibonacci.py
+++ fibonacci.py
@@ -6,12 +6,14 @@ fibonacci_sequence.append(a)
 fibonacci_sequence.append(b)
 
 print(fibonacci_sequence)
-index=input("What index would you like to see? ")
-# If you want to calculate a new number as the sum of the last two numbers
-# We can use slicing to sum the last two elements.
-for i in range(index):
-    new_num = sum(fibonacci_sequence[-2:])  # Slicing to get the last two elements
-print(new_num)
 
-fibonacci_sequence.append(new_num)  # Add the new number to the sequence
+# Ask the user for the index
+index = int(input("What index would you like to see? "))
+
+# Loop to generate the Fibonacci sequence up to the requested index
+for i in range(2, index):  # Start from 2 since the first two numbers are already in the list
+    new_num = sum(fibonacci_sequence[-2:])  # Sum the last two elements
+    fibonacci_sequence.append(new_num)  # Append the new number to the sequence
+
+# Show the final sequence up to the requested index
 print(fibonacci_sequence)
