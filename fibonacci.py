db56b5a2840addf0f9b23a23a977afeb6752ea6c
tomwarden <tomlukeprogramming@outlook.com>
2024-09-23 17:30:44 +0100

    Working on loop to continuously check

--- fibonacci.py
+++ fibonacci.py
@@ -1,13 +1,17 @@
 fibonacci_sequence = []
 
-a = 0; b = 1
-n = new_num
-
+a = 0
+b = 1
 fibonacci_sequence.append(a)
 fibonacci_sequence.append(b)
 
 print(fibonacci_sequence)
-
-new_num = sum(fibonacci_sequence(-n,n))
+index=input("What index would you like to see? ")
+# If you want to calculate a new number as the sum of the last two numbers
+# We can use slicing to sum the last two elements.
+for i in range(index):
+    new_num = sum(fibonacci_sequence[-2:])  # Slicing to get the last two elements
 print(new_num)
-fibonacci_sequence.append(new_num)
+
+fibonacci_sequence.append(new_num)  # Add the new number to the sequence
+print(fibonacci_sequence)
